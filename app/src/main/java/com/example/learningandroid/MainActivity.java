87150fcc914ac5cb3412d66e319eee4cbb97aeb7
Thays <thayscasado@hotmail.com>
2021-04-26 02:57:46 -0700

    Adding function to all buttons in a single method using interface and displaying a message on screen for each button

--- app/src/main/java/com/example/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Define data
     private TextView txtCounter;
@@ -56,45 +57,77 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
+        // Adding function to the buttons by implementing the class with the view on click
+        // Line 10 and 105
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
+
     }
 
     /// Adding function to the buttons using methods
-    public void plusFunction(View view) {
-        int counter = Integer.parseInt(txtCounter.getText().toString());
-        counter++;
-        txtCounter.setText(String.valueOf(counter));
-    }
-
-    public void minusFunction(View view) {
-        int counter = Integer.parseInt(txtCounter.getText().toString());
-        if (counter !=0){
-            counter--;
-            txtCounter.setText(String.valueOf(counter));
-        }
-    }
-    public void resetFunction(View view) {
-        txtCounter.setText(String.valueOf(0));
-    }
+//    public void plusFunction(View view) {
+//        int counter = Integer.parseInt(txtCounter.getText().toString());
+//        counter++;
+//        txtCounter.setText(String.valueOf(counter));
+//    }
+//
+//    public void minusFunction(View view) {
+//        int counter = Integer.parseInt(txtCounter.getText().toString());
+//        if (counter !=0){
+//            counter--;
+//            txtCounter.setText(String.valueOf(counter));
+//        }
+//    }
+//    public void resetFunction(View view) {
+//        txtCounter.setText(String.valueOf(0));
+//    }
 
     // Adding a function to all buttons
-    public void operation(View view) {
-        int id = view.getId();
+//    public void operation(View view) {
+//        int id = view.getId();
+//        int counter = Integer.parseInt(txtCounter.getText().toString());
+//
+//        switch (id){
+//            case R.id.btn_plus:
+//                counter++;
+//                txtCounter.setText(String.valueOf(counter));
+//                break;
+//            case R.id.btn_minus:
+//                if(counter != 0){
+//                    counter--;
+//                    txtCounter.setText(String.valueOf(counter));
+//                }
+//                break;
+//            case R.id.btn_reset:
+//                txtCounter.setText(String.valueOf(0));
+//                break;
+//        }
+//    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
         int counter = Integer.parseInt(txtCounter.getText().toString());
 
-        switch (id){
+        switch (id) {
             case R.id.btn_plus:
                 counter++;
                 txtCounter.setText(String.valueOf(counter));
+                Toast.makeText(this, "Plus Button is clicked", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_minus:
-                if(counter != 0){
+                if (counter != 0) {
                     counter--;
                     txtCounter.setText(String.valueOf(counter));
                 }
+                Toast.makeText(this, "Minus Button is clicked", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_reset:
                 txtCounter.setText(String.valueOf(0));
+                Toast.makeText(this, "Reset Button is clicked", Toast.LENGTH_SHORT).show();
                 break;
         }
+
     }
 }
